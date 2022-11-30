c114a1010fa238322a4397c00d395e9c6dc07e4e
ImSrishti <shristi998@gmail.com>
2022-11-30 19:38:23 +0530

    simple pagination added

--- src/components/spacex.js
+++ src/components/spacex.js
@@ -6,18 +6,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Pagination from '@mui/material/Pagination';
 import {
   Chip,
-  CircularProgress,
   FormControl,
   MenuItem,
   Select,
+  TablePagination,
 } from '@mui/material';
 
 export default function BasicTable() {
-  const [tabelData, setTableData] = useState([]);
-
+  const [tableData, setTableData] = useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
   useEffect(() => {
     var requestOptions = {
       method: 'GET',
@@ -42,10 +42,24 @@ export default function BasicTable() {
       })
       .catch((error) => console.log('error', error));
   }, []);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <>
-      <FormControl sx={{ marginLeft: 'auto' }}>
+      {/* <FormControl
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '100%',
+        }}
+      >
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
@@ -56,11 +70,18 @@ export default function BasicTable() {
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
-      </FormControl>
-      {/* <CircularProgress color="inherit" /> */}
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={10}
+          // onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl> */}
       <TableContainer component={Paper}>
-        {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
-
         <Table
           sx={{ minWidth: 650, borderRadius: 3 }}
           aria-label="simple table"
@@ -68,46 +89,55 @@ export default function BasicTable() {
           <TableHead sx={{ backgroundColor: 'gainsboro' }}>
             <TableRow>
               <TableCell>No</TableCell>
-              <TableCell align="right">Launched UTC</TableCell>
-              <TableCell align="right">Location</TableCell>
-              <TableCell align="right">Mission</TableCell>
-              <TableCell align="right">Orbit</TableCell>
-              <TableCell align="right">Launch Status</TableCell>
-              <TableCell align="right">Rocket</TableCell>
+              <TableCell align="left">Launched UTC</TableCell>
+              <TableCell align="left">Location</TableCell>
+              <TableCell align="left">Mission</TableCell>
+              <TableCell align="left">Orbit</TableCell>
+              <TableCell align="left">Launch Status</TableCell>
+              <TableCell align="left">Rocket</TableCell>
             </TableRow>
           </TableHead>
+
           <TableBody>
-            {tabelData.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.no}
-                </TableCell>
-                <TableCell align="right">{row.launched}</TableCell>
-                <TableCell align="right">{row.location}</TableCell>
-                <TableCell align="right">{row.mission}</TableCell>
-                <TableCell align="right">{row.orbit}</TableCell>
-                <TableCell align="center">
-                  {row.launchStatus ? (
-                    <Chip label="success" color="success" />
-                  ) : (
-                    <Chip label="failed" color="error" />
-                  )}
-                </TableCell>
-                <TableCell align="right">{row.rocket}</TableCell>
-              </TableRow>
-            ))}
+            {tableData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.no}
+                  </TableCell>
+                  <TableCell align="left">{row.launched}</TableCell>
+                  <TableCell align="left">{row.location}</TableCell>
+                  <TableCell align="left">{row.mission}</TableCell>
+                  <TableCell align="left">{row.orbit}</TableCell>
+                  <TableCell align="left">
+                    {row.launchStatus ? (
+                      <Chip label="success" color="success" />
+                    ) : (
+                      <Chip label="failed" color="error" />
+                    )}
+                  </TableCell>
+                  <TableCell align="left">{row.rocket}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <Pagination
+
+      <TablePagination
         sx={{ marginTop: 1, marginLeft: 'auto' }}
-        count={5}
-        variant="outlined"
-        shape="rounded"
+        component="div"
+        count={tableData.length}
+        rowsPerPage={10}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
       />
     </>
   );
 }
+
+//pagination https://www.youtube.com/watch?v=wAGIOCqS8tk
